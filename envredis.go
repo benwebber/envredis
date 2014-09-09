68c31714c75919d4453f6381d331d089e1381960
Ben Webber <benjamin.webber@gmail.com>
2014-09-08 21:37:53 -0400

    Separate CLI from nuts and bolts

--- envredis.go
+++ envredis.go
@@ -6,11 +6,9 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"regexp"
 	"strings"
 
-	"github.com/codegangsta/cli"
 	"github.com/fzzy/radix/redis"
 )
 
@@ -25,29 +23,6 @@ type Config struct {
 
 func NewConfig() *Config {
 	return &Config{}
-}
-
-func NewConfigFromCLIContext(ctx *cli.Context) *Config {
-	var command string
-	switch ctx.Command.Name {
-	case "list", "run":
-		command = "HGETALL"
-	case "get":
-		command = "HGET"
-	case "set":
-		command = "HSET"
-	case "delete":
-		command = "HDEL"
-	case "clear":
-		command = "DEL"
-	}
-	return &Config{
-		RedisURL: ctx.GlobalString("url"),
-		Command:  command,
-		Key:      ctx.GlobalString("key"),
-		Args:     ctx.Args(),
-		POSIX:    ctx.GlobalBool("posix"),
-	}
 }
 
 // Transform an environment variable name to follow the POSIX standard.
@@ -78,15 +53,7 @@ func redisCommand(config *Config) *redis.Reply {
 	return client.Cmd(config.Command, args)
 }
 
-// Start a process with environment variables from Redis.
-func runCommand(ctx *cli.Context) (ret int, err error) {
-	if len(ctx.Args()) == 0 {
-		log.Fatal("you must provide a command name")
-	}
-	config := NewConfigFromCLIContext(ctx)
-	// Remove arguments before passing to redis.Client, otherwise HGETALL will
-	// throw an error.
-	config.Args = []string{}
+func RunWithEnvVars(config *Config, command string, args ...string) (ret int, err error) {
 	// Load application configuration from Redis.
 	envConfig, err := redisCommand(config).Hash()
 	if err != nil {
@@ -105,7 +72,7 @@ func runCommand(ctx *cli.Context) (ret int, err error) {
 	}
 	// exec() the child process.
 	var child *exec.Cmd
-	child = exec.Command(ctx.Args()[0], ctx.Args()[1:]...)
+	child = exec.Command(command, args...)
 	child.Stdout = os.Stdout
 	child.Stderr = os.Stderr
 	child.Env = childEnv
@@ -116,9 +83,7 @@ func runCommand(ctx *cli.Context) (ret int, err error) {
 	return ret, err
 }
 
-// List all environment variables stored in Redis.
-func listCommand(ctx *cli.Context) (ret int, err error) {
-	config := NewConfigFromCLIContext(ctx)
+func ListEnvVar(config *Config) (ret int, err error) {
 	// Load application configuration from Redis.
 	envConfig, err := redisCommand(config).Hash()
 	// Output environment variables as key=value. Surround the values in quotes
@@ -135,12 +100,7 @@ func listCommand(ctx *cli.Context) (ret int, err error) {
 	return ret, err
 }
 
-// Retrieve a specific environment variable from Redis.
-func getCommand(ctx *cli.Context) (ret int, err error) {
-	config := NewConfigFromCLIContext(ctx)
-	if len(ctx.Args()) == 0 {
-		log.Fatal("you must provide a variable name")
-	}
+func GetEnvVar(config *Config) (ret int, err error) {
 	reply, err := redisCommand(config).Str()
 	if err != nil {
 		log.Fatal(err)
@@ -149,31 +109,10 @@ func getCommand(ctx *cli.Context) (ret int, err error) {
 	return ret, err
 }
 
-// Set a specific environment variable in Redis.
-func setCommand(ctx *cli.Context) (ret int, err error) {
-	var (
-		envvar string
-		value  string
-	)
-	if len(ctx.Args()) == 1 {
-		// set FOO=bar
-		args := strings.Split(ctx.Args()[0], "=")
-		if len(args) == 2 {
-			envvar, value = args[0], args[1]
-		} else {
-			log.Fatal("you must provide a variable name and value")
-		}
-	} else if len(ctx.Args()) >= 2 {
-		// set FOO bar
-		envvar, value = ctx.Args()[0], ctx.Args()[1]
-	} else {
-		log.Fatal("you must provide a variable name and value")
-	}
-	config := NewConfigFromCLIContext(ctx)
+func SetEnvVar(config *Config) (ret int, err error) {
 	if config.POSIX {
-		envvar = makePOSIXCompatible(envvar)
+		config.Args[0] = makePOSIXCompatible(config.Args[0])
 	}
-	config.Args = []string{envvar, value}
 	_, err = redisCommand(config).Int()
 	if err != nil {
 		log.Fatal(err)
@@ -181,12 +120,7 @@ func setCommand(ctx *cli.Context) (ret int, err error) {
 	return ret, err
 }
 
-// Delete an environment variable from Redis.
-func deleteCommand(ctx *cli.Context) (ret int, err error) {
-	if len(ctx.Args()) == 0 {
-		log.Fatal("you must provide a variable name")
-	}
-	config := NewConfigFromCLIContext(ctx)
+func DeleteEnvVar(config *Config) (ret int, err error) {
 	_, err = redisCommand(config).Int()
 	if err != nil {
 		log.Fatal(err)
@@ -194,90 +128,10 @@ func deleteCommand(ctx *cli.Context) (ret int, err error) {
 	return ret, err
 }
 
-// Clear an application's environment variables from Redis.
-func clearCommand(ctx *cli.Context) (ret int, err error) {
-	config := NewConfigFromCLIContext(ctx)
+func ClearEnvVar(config *Config) (ret int, err error) {
 	ret, err = redisCommand(config).Int()
 	if err != nil {
 		log.Fatal(err)
 	}
 	return ret, err
-}
-
-func main() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		os.Exit(1)
-	}
-	app := cli.NewApp()
-	app.Name = "envredis"
-	app.Version = "0.1.0"
-	app.Usage = "Load process environments from Redis."
-	app.Action = func(ctx *cli.Context) {
-		runCommand(ctx)
-	}
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "url, u",
-			Value:  "redis://localhost:6379",
-			Usage:  "Redis connection URL",
-			EnvVar: "ENVREDIS_REDIS_URL",
-		},
-		cli.StringFlag{
-			Name:   "key, k",
-			Value:  filepath.Base(pwd),
-			Usage:  "name of Redis hash storing configuration",
-			EnvVar: "ENVREDIS_REDIS_KEY",
-		},
-		cli.BoolFlag{
-			Name:   "posix",
-			Usage:  "make all variable names follow the POSIX standard",
-			EnvVar: "ENVREDIS_POSIX",
-		},
-	}
-	app.Commands = []cli.Command{
-		{
-			Name:  "run",
-			Usage: "run a command",
-			Action: func(ctx *cli.Context) {
-				runCommand(ctx)
-			},
-		},
-		{
-			Name:  "list",
-			Usage: "list environment variables",
-			Action: func(ctx *cli.Context) {
-				listCommand(ctx)
-			},
-		},
-		{
-			Name:  "get",
-			Usage: "get an environment variable",
-			Action: func(ctx *cli.Context) {
-				getCommand(ctx)
-			},
-		},
-		{
-			Name:  "set",
-			Usage: "set an environment variable",
-			Action: func(ctx *cli.Context) {
-				setCommand(ctx)
-			},
-		},
-		{
-			Name:  "delete",
-			Usage: "delete an environment variable",
-			Action: func(ctx *cli.Context) {
-				deleteCommand(ctx)
-			},
-		},
-		{
-			Name:  "clear",
-			Usage: "clear all environment variables",
-			Action: func(ctx *cli.Context) {
-				clearCommand(ctx)
-			},
-		},
-	}
-	app.Run(os.Args)
 }
---
+++ main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/codegangsta/cli"
+)
+
+func NewConfigFromCLIContext(ctx *cli.Context) *Config {
+	var command string
+	args := ctx.Args()
+	switch ctx.Command.Name {
+	case "list":
+		command = "HGETALL"
+	case "get":
+		command = "HGET"
+	case "set":
+		command = "HSET"
+	case "delete":
+		command = "HDEL"
+	case "clear":
+		command = "DEL"
+	default:
+		command = "HGETALL"
+	}
+	if command == "HGETALL" {
+		// Remove arguments before passing to redis.Client, otherwise HGETALL
+		// will throw an error.
+		args = []string{}
+	}
+	return &Config{
+		RedisURL: ctx.GlobalString("url"),
+		Command:  command,
+		Key:      ctx.GlobalString("key"),
+		Args:     args,
+		POSIX:    ctx.GlobalBool("posix"),
+	}
+}
+
+// Start a process with environment variables from Redis.
+func runCommand(ctx *cli.Context) (ret int, err error) {
+	if len(ctx.Args()) == 0 {
+		log.Fatal("you must provide a command name")
+	}
+	config := NewConfigFromCLIContext(ctx)
+	return RunWithEnvVars(config, ctx.Args()[0], ctx.Args()[1:]...)
+}
+
+// List all environment variables stored in Redis.
+func listCommand(ctx *cli.Context) (ret int, err error) {
+	config := NewConfigFromCLIContext(ctx)
+	return ListEnvVar(config)
+}
+
+// Retrieve a specific environment variable from Redis.
+func getCommand(ctx *cli.Context) (ret int, err error) {
+	if len(ctx.Args()) == 0 {
+		log.Fatal("you must provide a variable name")
+	}
+	config := NewConfigFromCLIContext(ctx)
+	return GetEnvVar(config)
+}
+
+// Set a specific environment variable in Redis.
+func setCommand(ctx *cli.Context) (ret int, err error) {
+	var (
+		envvar string
+		value  string
+	)
+	if len(ctx.Args()) == 1 {
+		// set FOO=bar
+		args := strings.Split(ctx.Args()[0], "=")
+		if len(args) == 2 {
+			envvar, value = args[0], args[1]
+		} else {
+			log.Fatal("you must provide a variable name and value")
+		}
+	} else if len(ctx.Args()) >= 2 {
+		// set FOO bar
+		envvar, value = ctx.Args()[0], ctx.Args()[1]
+	} else {
+		log.Fatal("you must provide a variable name and value")
+	}
+	config := NewConfigFromCLIContext(ctx)
+	config.Args = []string{envvar, value}
+	return SetEnvVar(config)
+}
+
+// Delete an environment variable from Redis.
+func deleteCommand(ctx *cli.Context) (ret int, err error) {
+	if len(ctx.Args()) == 0 {
+		log.Fatal("you must provide a variable name")
+	}
+	config := NewConfigFromCLIContext(ctx)
+	return DeleteEnvVar(config)
+}
+
+// Clear an application's environment variables from Redis.
+func clearCommand(ctx *cli.Context) (ret int, err error) {
+	config := NewConfigFromCLIContext(ctx)
+	return ClearEnvVar(config)
+}
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		os.Exit(1)
+	}
+	app := cli.NewApp()
+	app.Name = "envredis"
+	app.Version = "0.1.0"
+	app.Usage = "Load process environments from Redis."
+	app.Action = func(ctx *cli.Context) {
+		runCommand(ctx)
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "url, u",
+			Value:  "redis://localhost:6379",
+			Usage:  "Redis connection URL",
+			EnvVar: "ENVREDIS_REDIS_URL",
+		},
+		cli.StringFlag{
+			Name:   "key, k",
+			Value:  filepath.Base(pwd),
+			Usage:  "name of Redis hash storing configuration",
+			EnvVar: "ENVREDIS_REDIS_KEY",
+		},
+		cli.BoolFlag{
+			Name:   "posix",
+			Usage:  "make all variable names follow the POSIX standard",
+			EnvVar: "ENVREDIS_POSIX",
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "run a command",
+			Action: func(ctx *cli.Context) {
+				runCommand(ctx)
+			},
+		},
+		{
+			Name:  "list",
+			Usage: "list environment variables",
+			Action: func(ctx *cli.Context) {
+				listCommand(ctx)
+			},
+		},
+		{
+			Name:  "get",
+			Usage: "get an environment variable",
+			Action: func(ctx *cli.Context) {
+				getCommand(ctx)
+			},
+		},
+		{
+			Name:  "set",
+			Usage: "set an environment variable",
+			Action: func(ctx *cli.Context) {
+				setCommand(ctx)
+			},
+		},
+		{
+			Name:  "delete",
+			Usage: "delete an environment variable",
+			Action: func(ctx *cli.Context) {
+				deleteCommand(ctx)
+			},
+		},
+		{
+			Name:  "clear",
+			Usage: "clear all environment variables",
+			Action: func(ctx *cli.Context) {
+				clearCommand(ctx)
+			},
+		},
+	}
+	app.Run(os.Args)
+}
